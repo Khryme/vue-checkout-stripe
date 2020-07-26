fe926cdaae029acc5012e1070525dbc758cb9cfa
fabrizio.stella <fabrizio.stella343@gmail.com>
2020-07-26 23:09:09 +0200

    prettier

--- src/stripe/model/stripe.model.ts
+++ src/stripe/model/stripe.model.ts
@@ -1,21 +1,21 @@
 export const COMPONENT_STYLE = {
     base: {
-        fontSize: '13px',
-        fontSmoothing: 'antialiased',
-        fontWeight: '400',
-        fontFamily: 'Arial'
+        fontSize: "13px",
+        fontSmoothing: "antialiased",
+        fontWeight: "400",
+        fontFamily: "Arial"
     },
     complete: {}
-}
+};
 
 export const COMPONENT_OPTIONS = {
     style: COMPONENT_STYLE,
     classes: {
-        base: 'form-stripe-element',
-        focus: 'form-stripe-element--focus',
-        disabled: 'form-stripe-element--disabled',
-        complete: 'form-stripe-element--is-success',
-        invalid: 'form-stripe-element--is-error',
-        empty: 'form-stripe-element'
+        base: "form-stripe-element",
+        focus: "form-stripe-element--focus",
+        disabled: "form-stripe-element--disabled",
+        complete: "form-stripe-element--is-success",
+        invalid: "form-stripe-element--is-error",
+        empty: "form-stripe-element"
     }
-}
+};
